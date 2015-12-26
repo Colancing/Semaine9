0860cab369f40c27455fb8db690d5e879000b89d
Emmanuelle Hommet <pseudoups@hotmail.fr>
2015-12-26 19:34:04 +0000

    debut exercices  tableau

--- Tableau/javascript.js
+++ Tableau/javascript.js
@@ -1,1 +1,11 @@
 // JavaScript File
+
+afficherListe(){
+    var tableau= ['John', 'henri', 'Jospehine', 'Lucas'];
+    for (id=0; id < tableau.length, i++){
+       return  tableau[id]
+    }
+    
+    
+    
+}
