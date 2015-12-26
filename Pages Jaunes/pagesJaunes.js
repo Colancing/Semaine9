6dd435efa4a2dc5c8e0c0f6f986427ac2037b05f
Emmanuelle Hommet <pseudoups@hotmail.fr>
2015-12-26 19:22:40 +0000

    creation des fichiers d'exercices pour semaine 9

---
+++ Pages Jaunes/pagesJaunes.js
@@ -0,0 +1,19 @@
+contacts = [{
+  prenom: "Julien",
+  nom: "Grillot",
+  numero: "0123456789"
+}, {
+  prenom: "John",
+  nom: "Smith",
+  numero: "0123456789"
+}];
+
+function texteContact(contact) {
+    prenom= prenom.this;
+    nom= nom.this;
+    numero=numero.this;
+    
+  // ...
+}
+
+alert(texteContact(contacts[0])); // affiche « Julien Grillot (0123456789) »
